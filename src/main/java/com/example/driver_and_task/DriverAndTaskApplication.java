fed7cc9266555620eeb93058ab820010480876b5
Sajer24 <sajer.kanat1992@gmail.com>
2023-07-05 23:07:19 +0300

    Initial commit

---
+++ src/main/java/com/example/driver_and_task/DriverAndTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.driver_and_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriverAndTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriverAndTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/driver_and_task/DriverAndTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.driver_and_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriverAndTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
