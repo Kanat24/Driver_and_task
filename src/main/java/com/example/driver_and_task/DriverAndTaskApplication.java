d7d679bc58b666520ddd3dfe5eb1e0f62e0f9f44
Sajer24 <sajer.kanat1992@gmail.com>
2023-07-05 23:15:30 +0300

    final version

--- src/main/java/com/example/driver_and_task/DriverAndTaskApplication.java
+++ src/main/java/com/example/driver_and_task/DriverAndTaskApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DriverAndTaskApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DriverAndTaskApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DriverAndTaskApplication.class, args);
+    }
 
 }
--- src/test/java/com/example/driver_and_task/DriverAndTaskApplicationTests.java
+++ src/test/java/com/example/driver_and_task/DriverAndTaskApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DriverAndTaskApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
